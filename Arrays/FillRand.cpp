70d54bb470c1ef256b1dc5c4233cd3dd37d92bc2
KseniyaPavlova <101828897+KseniyaPavlova@users.noreply.github.com>
2022-04-13 21:41:56 +0300

    Separation DONE

---
+++ Arrays/FillRand.cpp
@@ -0,0 +1,42 @@
+//#include"Constants.h"
+#include"FillRand.h"
+#include"stdafx.h"
+
+void FillRand(int arr[], const int n, int minRand, int maxRand)
+{
+	for (int i = 0; i < n; i++)
+	{
+		arr[i] = rand() % (maxRand - minRand) + minRand;
+	}
+}
+void FillRand(double arr[], const int n, int minRand, int maxRand)
+{
+
+	for (int i = 0; i < n; i++)
+	{
+		maxRand *= 100;
+		minRand *= 100;
+		arr[i] = rand() % (maxRand - minRand) + minRand;
+		arr[i] /= 100;
+	}
+}
+void FillRand(int arr[ROWS][COLS], const int ROWS, const int COLS, int minRand, int maxRand)
+{
+	for (int i = 0; i < ROWS; i++)
+	{
+		for (int j = 0; j < COLS; j++)
+		{
+			arr[i][j] = rand() % (maxRand - minRand) + minRand;
+		}
+	}
+}
+void FillRand(double arr[ROWS][COLS], const int ROWS, const int COLS, int minRand, int maxRand)
+{
+	for (int i = 0; i < ROWS; i++)
+	{
+		for (int j = 0; j < COLS; j++)
+		{
+			arr[i][j] = rand() % (maxRand - minRand) + minRand;
+		}
+	}
+}
---
+++ Arrays/Shift.h
@@ -0,0 +1,7 @@
+#pragma once
+#include"Constants.h"
+
+void shiftLeft(int arr[], const int n, int number_of_shifts);
+void shiftLeft(double arr[], const int n, int number_of_shifts);
+void shiftRight(int arr[], const int n, int number_of_shifts);
+void shiftRight(double arr[], const int n, int number_of_shifts); int minValueIn(int arr[], const int n);
